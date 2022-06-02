76acf917a907203bc5df2bb06ff21ae62d19ee40
AnupK-GH <aak18@live.com>
2022-06-02 11:08:36 -0400

    update to structure, index.html, and main.js

---
+++ frontend/main.js
@@ -0,0 +1,21 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    getVisitCount();  //function gets called
+});
+
+
+const functionApi = '';
+
+const getVisitCount = () => {
+    let count = 30;            //variable to hold the count
+    fetch(functionApi).then(response => {
+        return response.json()    //get data from URL
+    })
+    .then(response => {
+        console.log("Website called function API.");  //debugging
+        count = response.count;    //set variable to json response
+        document.getElementById('counter').innerText = count;  //go to html document, find element, and set element
+    }).catch(function(error) {     
+        console.log(error);     //if there is an error, log it to console
+      });
+    return count;
+}
